6f115139d01ba5684e4053965a6143325c896e05
Conner Lane <cal0043@auburn.edu>
2017-09-08 15:23:08 -0500

    another babylonian refactor

--- babylonian/babylonian.py
+++ babylonian/babylonian.py
@@ -7,10 +7,10 @@ for i in range(0, lines):
     power = 0
     while number_arr:
         n = number_arr.pop()
-        if not n:
+        if n:
+            decimal += (eval(n) * math.pow(60, power))
             power += 1
-            continue
-        decimal += (eval(n) * math.pow(60, power))
-        power += 1
+        else:
+            power += 1
 
     print(int(decimal))
