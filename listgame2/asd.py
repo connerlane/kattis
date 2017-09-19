fc1f8642798b0b4cb65fa188c93869f4b65d54f0
Conner Lane <cal0043@auburn.edu>
2017-09-19 15:05:37 -0500

    clean up listgame2 folder

--- listgame2/asd.py
+++
@@ -1,9 +0,0 @@
-x = int(raw_input())
-result = []
-i = 2
-while i <= x:
-    if x % i == 0:
-        result.append(i)
-        x = x / i
-    i = i + 1
-print len(result), result
--- listgame2/compare_algs2.py
+++
@@ -1,6 +0,0 @@
-import listgame2
-import approach2
-
-
-print(listgame2.solve(1000))
-print(approach2.solve(1000))
