2938bb069be13245f3f4b198b111a6478c165669
Conner Lane <cal0043@auburn.edu>
2017-09-22 11:19:30 -0500

    correct bing.py

--- bing/bing.py
+++ bing/bing.py
@@ -1,41 +1,29 @@
 class tree_node:
 
-    def __init__(self, character, parent, end_of_word=False):
+    def __init__(self, character, parent, end_of_word=0):
         self.character = character
         self.parent = parent
-        self.end_of_word = False
+        self.end_of_word = 0
         self.children = dict()
 
     def add_child(self, child):
         self.children[child] = None
 
     def __str__(self):
-        return(self.character + " " + str(self.parent))
+        return self.character + " " + str(self.parent)
 
-    def __hash__(self):
-        return hash((self.character))
 
 class prefix_tree:
     def __init__(self):
         self.tree = tree_node("", None)
 
     def get_all_children(self, node, all_children):
-        if node.end_of_word:
-            d = node
-            word = []
-            while d.parent:
-                word.append(d.character)
-                d = d.parent
-            word_string = "".join(reversed(word))
-            all_children.append(word_string)
+        if node.end_of_word > 0:
+            all_children[0] += node.end_of_word
         for k in node.children:
             c = node.children[k]
             self.get_all_children(c, all_children)
         return all_children
-
-
-
-
 
     def contains(self, word):
         word_list = list(word)
@@ -44,26 +32,23 @@ class prefix_tree:
         while x < len(word_list):
             if word_list[x] not in level.children:
                 for i in range(x, len(word_list)):
-                    level.children[word_list[i]] = tree_node(word_list[i], level)
+                    level.children[word_list[i]] = tree_node(
+                        word_list[i], level)
                     level = level.children[word_list[i]]
-                level.end_of_word = True
-                return []
+                level.end_of_word += 1
+                return [0]
             level = level.children[word_list[x]]
             x += 1
         level_pointer = level
-        child_words = self.get_all_children(level, [])
-        level_pointer.end_of_word = True
+        child_words = self.get_all_children(level, [0])
+        level_pointer.end_of_word += 1
         return child_words
-            
 
 
 if __name__ == "__main__":
     number_of_lines = int(input())
-    
+
     t = prefix_tree()
     for x in range(0, number_of_lines):
-        if x == 9:
-            q = 2
-        word = input()
-        print(len(t.contains(word)))
-    # print(t.contains(""))
+        input_word = input()
+        print(t.contains(input_word)[0])
